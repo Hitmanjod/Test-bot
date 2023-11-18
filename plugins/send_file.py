7bd6e965d2bad8cfbe0b2fd3cbd03f274569a4ec
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2023-11-18 14:56:01 +0545

    Update send_file.py

--- plugins/send_file.py
+++ plugins/send_file.py
@@ -1,4 +1,4 @@
-# © Telegram @HMF_Owner_1, GitHub @ThiruXD 
+# ©Telegram @Sujan_BotZ 
 
 import asyncio
 from configs import Config
@@ -37,4 +37,9 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    await asyncio.sleep(2)
+    await reply_forward(message=sent_message, file_id=file_id)
+    asyncio.create_task(delete_after_delay(sent_message, 43200))
+
+async def delete_after_delay(message, delay):
+    await asyncio.sleep(delay)
+    await message.delete()
