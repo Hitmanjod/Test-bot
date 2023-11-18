4a01372039048f55835a644c848f9001b525eac7
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2023-11-18 14:33:21 +0545

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# © Telegram @HMF_Owner_1, GitHub @ThiruXD 
-
 import os
 import time 
 
@@ -7,25 +5,25 @@ import time
 class Config(object):
 	API_ID = int(os.environ.get("API_ID", "25502576"))
 	API_HASH = os.environ.get("API_HASH", "f0f35dbb5b0081cdc8d3c9d5383c4628")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6449209866:AAG9QJDwNhJI14rXM2zMVvt7yIO_F8fsphY")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "TG_File_Store_Ro_Bot")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001936300025"))
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6369183180:AAE8nXrcaNI-ZPMtdn4AxI6Uf99wZ-6BWt0")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VnshortenerFileStoreBot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001948924720"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5123039648"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SujanC7:SujanC7@cluster0.vst9zln.mongodb.net/?retryWrites=true&w=majority")
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://HitmanLaude12:HitmanLaude12@cluster0.logbo9e.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001978535504")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001948924720")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	BASE_SITE = os.environ.get("BASE_SITE", "")
-	DOMAIN = os.environ.get("DOMAIN", "")
+	BASE_SITE = os.environ.get("BASE_SITE", "vnshortener.com")
+	DOMAIN = os.environ.get("DOMAIN", "vnshortener.com")
 	ABOUT_BOT_TEXT = f"""
 This is Public Files Store Bot !
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
-🤖 **My Name:** [File Store Bot](https://t.me/TG_File_Store_Ro_Bot)
+🤖 **My Name:** [Vn File Store Bot](https://t.me/VnshortenerFileStoreBot)
 
 📝 **Language:** [Python3](https://www.python.org)
 
@@ -33,18 +31,18 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-👑 **Owner:** @Sujan_Ch
+👑 **Owner:** Deleted Account
 
 📢 **Updates Channel:** @Sujan_BotZ 
 """
 	ABOUT_DEV_TEXT = f"""
-**🌐 This Bot Was Devloped By** : @Sujan_Ch"""
+**🌐 This Bot Was Devloped By** : @Sujan_BotZ"""
 	
 PREFIX = ["/", ".", "?", "#", "@", "₹", "+", ":", "!", "^", "|"]
-START_MEDIA = "https://graph.org/file/a18cf9f447a1c34e5a20a.jpg"
+START_MEDIA = "https://graph.org/file/6b16ad03f00948d2d719e.jpg"
 START_TEXT = """Hɪ/Hᴇʟʟᴏ [{}](tg://user?id={})
 
-This is a Permanent FileStore Bot.
+This is a Vnshortener FileStore Bot.
 
 How to Use Bot & it's Benefits??
 
@@ -54,21 +52,21 @@ How to Use Bot & it's Benefits??
 
 ❌ PORNOGRAPHY CONTENTS are strictly prohibited & get Permanent Ban.
 
-Pᴏᴡᴇʀᴇᴅ Bʏ : [Sujan_Ch](http://Sujan_BotZ)"""
+Pᴏᴡᴇʀᴇᴅ Bʏ : [Sujan_BotZ](http://Sujan_BotZ)"""
 
 HELP_TEXT = """Hᴏᴡ Tᴏ Verify:
 
-Sᴛᴇᴘ Nᴏ 1 : Just Copy This (6c5db31980885e46221e90106f1d47b8295aa0f8) Token.
+Sᴛᴇᴘ Nᴏ 1 : Just Copy Your Api Token From Website.
 
 Sᴛᴇᴘ Nᴏ 2 : Then Use /verify Paste Token Here.
 
 Exᴀᴍᴘʟᴇ : /verify ```6c5db31980885e46221e90106f1d47b8295aa0f8``` """
 
-ABOUT_TEXT = """🤖 Name : File Store Bot 
+ABOUT_TEXT = """🤖 Name : Vnshortener File Store Bot 
 
 🔠 Language  : Python3
 📚 Library   : Teleton And Pyrogram
-👑 Owner     : @sujan_ch
+👑 Owner     : Deleted Account 
 
 ©️Powered By @Sujan_BotZ """
 
